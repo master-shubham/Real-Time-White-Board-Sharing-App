6f9f69e2dc4dc608b5b4e48881a74a00cb289b77
Master Shubham <ommylight@gmail.com>
2026-05-16 09:25:09 +0530

    feat: setup the backend structure

---
+++ backend/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import http from "http";
+import { Server, Socket } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // In production, replace with your specific frontend URL
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("This is MERN realtime whiteboard sharing app.");
+});
+
+io.on("connection", (socket: Socket) => {
+  console.log(`User connected: ${socket.id}`);
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
